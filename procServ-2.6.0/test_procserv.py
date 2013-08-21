028294ff14276542e3d052d5162a2d4ce3a7abb0
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2013-08-21 09:41:08 +0000

    add test script
    Former-commit-id: b2d36192dc8a00625bc2faa7b2c2187095d58ebd

---
+++ procServ-2.6.0/test_procserv.py
@@ -0,0 +1,26 @@
+import telnetlib
+import time
+tn1=telnetlib.Telnet('localhost',20000)
+while True :
+    for i in range(10) :
+        tn1.read_very_eager()
+        tn1.write('dbl\n')
+        time.sleep(2)
+        tn1.write('\n\n\n\n\n\n')
+        time.sleep(1)
+    # connect to main port (r/w)
+    tn=telnetlib.Telnet('localhost',20000)
+    for i in range(5) :
+        tn.read_very_eager()
+        tn.write('dbl\n')
+        time.sleep(2)
+        tn.write('\n\n\n\n\n\n')
+        time.sleep(1)
+    # connect to log port (ro)
+    tn2=telnetlib.Telnet('localhost',20001)
+    time.sleep(3)
+    tn2.read_very_eager()
+    time.sleep(1)
+    tn2.close()
+    tn.close()
+tn1.close()
