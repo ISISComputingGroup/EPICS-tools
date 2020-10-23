4113ce2cf205e10c6c28ee200abb55a6aacebefe
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-10-23 21:23:59 +0100

    Update jenkins_build.bat

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -5,3 +5,5 @@ c:\cygwin64\bin\bash.exe --login -c "cd %CURRENT%; sh build.sh"
 cd ..\conserver-8.1.19
 for /F "usebackq" %%I in (`c:\cygwin64\bin\cygpath --absolute .`) do SET CURRENT=%%I
 c:\cygwin64\bin\bash.exe --login -c "cd %CURRENT%; sh build.sh"
+robocopy "cygwin_bin" "\\isis\inst$\Kits$\CompGroup\ICP\EPICS_Tools\cygwin_bin" /E /PURGE
+robocopy "cygwin_bin64" "\\isis\inst$\Kits$\CompGroup\ICP\EPICS_Tools\cygwin_bin64" /E /PURGE
