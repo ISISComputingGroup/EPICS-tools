57f31692c07d5632f517335cfd5028a83ab7b439
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2021-01-10 15:15:37 +0000

    Copy additional cygwin utilities

--- procServ-2.6.0/build.sh
+++ procServ-2.6.0/build.sh
@@ -7,9 +7,13 @@ make
 mkdir -p ../cygwin_bin ../cygwin_bin64
 cp -f procServ.exe ../cygwin_bin
 cp -f procServ.exe ../cygwin_bin64
-cp -f /usr/bin/kill.exe ../cygwin_bin/cygwin_kill.exe
-cp -f /usr/bin/kill.exe ../cygwin_bin64/cygwin_kill.exe
-for i in cygwin1.dll cygpath.exe; do
+# these are used for managing process and PID file 
+for i in kill.exe rm.exe ps.exe; do
+    cp -f /usr/bin/$i ../cygwin_bin/cygwin_$i
+    cp -f /usr/bin/$i ../cygwin_bin64/cygwin_$i
+done
+# cygintl-8.dll is needed by rm.exe
+for i in cygwin1.dll cygintl-8.dll cygpath.exe; do
     cp -f /usr/bin/$i ../cygwin_bin
     cp -f /usr/bin/$i ../cygwin_bin64
 done
