6e71e467a4f342e0cda39d88e9c20e745eae934d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-01-17 23:23:52 +0000

    Use full network path

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -8,8 +8,8 @@ for /F "usebackq" %%I in (`c:\cygwin64\bin\cygpath --absolute .`) do SET CURRENT
 c:\cygwin64\bin\bash.exe --login -c "cd %CURRENT%; sh build.sh"
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 cd ..
-robocopy "cygwin_bin" "\\isis\inst$\Kits$\CompGroup\ICP\EPICS_Tools\cygwin_bin" /E /PURGE /NFL /NDL /NP
+robocopy "cygwin_bin" "\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\EPICS_Tools\cygwin_bin" /E /PURGE /NFL /NDL /NP
 if %ERRORLEVEL% GEQ 4 exit /b %ERRORLEVEL%
-robocopy "cygwin_bin64" "\\isis\inst$\Kits$\CompGroup\ICP\EPICS_Tools\cygwin_bin64" /E /PURGE /NFL /NDL /NP
+robocopy "cygwin_bin64" "\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\EPICS_Tools\cygwin_bin64" /E /PURGE /NFL /NDL /NP
 if %ERRORLEVEL% GEQ 4 exit /b %ERRORLEVEL%
 exit /b 0
