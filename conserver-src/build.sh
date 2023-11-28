fa8527016939d34d60fdb77491746105f0708512
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-11-28 14:44:12 +0000

    Update build.sh

--- conserver-src/build.sh
+++ conserver-src/build.sh
@@ -8,7 +8,7 @@ make || make
 for i in conserver/conserver.exe console/console.exe conserver.cf/samples/basic.cf; do
     cp -f $i ../cygwin/bin
 done
-for i in cygcrypto-1.1.dll cygcrypt-0.dll cygcrypt-2.dll cygssl-1.1.dll cygz.dll; do
+for i in cygcrypto-1.1.dll cygcrypt-0.dll cygcrypt-2.dll cygssl-1.1.dll cygz.dll cygwrap-0.dll; do
     if test ! -e ../cygwin/bin/$i; then
 	    echo Copying $i
 		cp -f /bin/$i ../cygwin/bin
