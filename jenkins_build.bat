320ac3b8e65150f6ee5df6e9c64e2c830361d803
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-12-01 17:41:03 +0000

    Update jenkins_build.bat

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -1,7 +1,7 @@
 setlocal
 REM this is a minimal cygwin install used at ibex runtime
 REM we install procServ and conserver into this directory
-robocopy "c:\mini_cygwin64" "%~dp0cygwin" /E /PURGE /NFL /NDL /NP /XF "rebase.db.x86_64" /R:5
+robocopy "c:\mini_cygwin64" "%~dp0cygwin" /E /PURGE /NFL /NDL /NP /XF "rebase.db.x86_64" /XD "c:\mini_cygwin64\home\gamekeeper" /R:5
 if %ERRORLEVEL% GEQ 4 exit /b %ERRORLEVEL%
 "%~dp0build_utils\sed.exe" -i -e "s/^none \/cygdrive.*/none \/cygdrive cygdrive binary,noacl,posix=0,user 0 0/" "%~dp0cygwin\etc\fstab"
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
