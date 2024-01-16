e39a5ffb9e867d0fb720f6894e55527d87ab9ed1
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-01-16 12:11:12 +0000

    Disable ASLR on cygwin1.dll

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -3,6 +3,8 @@ REM this is a minimal cygwin install used at ibex runtime
 REM we install procServ and conserver into this directory
 robocopy "c:\mini_cygwin64" "%~dp0cygwin" /E /PURGE /NFL /NDL /NP /XF "rebase.db.x86_64" /XD "c:\mini_cygwin64\home\gamekeeper" /R:5
 if %ERRORLEVEL% GEQ 4 exit /b %ERRORLEVEL%
+REM disable ASLR for cygwin DLL
+c:\cygwin64\bin\bash.exe --login -c "peflags --dynamicbase 0 `cygpath --absolute '%~dp0cygwin\bin\cygwin1.dll'`"
 "%~dp0build_utils\sed.exe" -i -e "s/^none \/cygdrive.*/none \/cygdrive cygdrive binary,noacl,posix=0,user 0 0/" "%~dp0cygwin\etc\fstab"
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 REM the full cygwin with compiler packages is in c:\cygwin64
