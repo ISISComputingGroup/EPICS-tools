6b8753983dc5270c81200cb11b15a7d92caacb01
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2025-02-19 17:27:59 +0000

    Increase wait

--- start_cygserver.bat
+++ start_cygserver.bat
@@ -11,6 +11,7 @@ if not exist "%~dp0cygwin\etc\cygserver.conf" (
     copy %~dp0cygwin\etc\defaults\etc\cygserver.conf %~dp0cygwin\etc\cygserver.conf
 )
 powershell -Command "Start-Process cmd -Args /c,\"%~dp0cygwin\usr\sbin\cygserver.exe --stderr --no-syslog\" -RSE C:\Instrument\Var\logs\ibex_server\cygserver_err.log -RSO C:\Instrument\Var\logs\ibex_server\cygserver_out.log -WindowStyle Hidden"
-REM we started using cygserver due to an ldap issue - give it time to setup
-@echo %DATE% %TIME% waiting 30 seconds for cygserver to complete setup
-waitfor /t 30 WillNeverHappen >NUL 2>&1
+REM we started using cygserver due to a site ldap issue - seems to need at least 45 seconds
+REM to do and cache this lookup
+@echo %DATE% %TIME% waiting 60 seconds for cygserver to complete setup
+waitfor /t 60 WillNeverHappen >NUL 2>&1
